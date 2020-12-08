d4b748427737d0463e0abc55f2fd8c952fb07413
Leah Arden <leaharden175@gmail.com>
2020-12-08 17:35:57 -0500

    drill 4 done

--- drills.js
+++ drills.js
@@ -29,3 +29,10 @@ function dewey(data, title, start, end) {
   
 
 //DRILL 4
+
+//in-order     14 15 19 25 27 35 79 89 90 91
+//pre-order    35 25 15 14 19 27 89 79 91 90
+//post-order   14 15 19 25 27 79 89 90 91 35
+
+//post-order   5 7 6 9 11 10 8
+//pre-order    8 5 7 6 9 11 10
