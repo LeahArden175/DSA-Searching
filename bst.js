629d86ccd52323f489c51dbac51ea8f261ee4f59
Leah Arden <leaharden175@gmail.com>
2020-12-08 14:31:53 -0500

    initial commit

---
+++ bst.js
@@ -0,0 +1,139 @@
+const Queue = require("./queue");
+
+class BinarySearchTree {
+  constructor(key = null, value = null, parent = null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value) {
+    //if the tree if empty then this key will be the root
+    if (this.key == null) {
+      this.key = key;
+      this.value = value;
+    } else if (key < this.key) {
+      /* If the tree already exists, then start at the root, 
+           and compare it to the key you want to insert.
+           If the new key is less than the node's key 
+           then the new node needs to live in the left-hand branch */
+      if (this.left == null) {
+        this.left = new BinarySearchTree(key, value, this);
+      } else {
+        /* If the node has an existing left child, 
+               then we recursively call the `insert` method 
+               so the node is added further down the tree */
+        this.left.insert(key, value);
+      }
+    } else {
+      /* Similarly, if the new key is greater than the node's key 
+           then you do the same thing, but on the right-hand side */
+      if (this.right == null) {
+        this.right = new BinarySearchTree(key, value, this);
+      } else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+  find(key) {
+    if (this.key == key) {
+      return this.value;
+    } else if (key < this.key && this.left) {
+      return this.left.find(key);
+    } else if (key > this.key && this.right) {
+      return this.right.find(key);
+    } else {
+      throw new Error("Key Error");
+    }
+  }
+
+  remove(key) {
+    if (this.key == key) {
+      if (this.left && this.right) {
+        const successor = this.right.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      } else if (this.left) {
+        this._replaceWith(this.left);
+      } else if (this.right) {
+        this._replaceWith(this.right);
+      } else {
+        this._replaceWith(null);
+      }
+    } else if (key < this.key && this.left) {
+      this.left.remove(key);
+    } else if (key > this.key && this.right) {
+      this.right.remove(key);
+    } else {
+      throw new Error("Key Error");
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this == this.parent.left) {
+        this.parent.left = node;
+      } else if (this == this.parent.right) {
+        this.parent.right = node;
+      }
+      if (node) {
+        node.parent = this.parent;
+      }
+    } else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      } else {
+        this.key = null;
+        this.value = null;
+        this.left = null;
+        this.right - null;
+      }
+    }
+  }
+
+  _findMin() {
+    if (!this.left) {
+      return this;
+    }
+    return this.left._findMin();
+  }
+
+  dfs(values = []) {
+    if (this.left) {
+      values = this.left.dfs(values);
+    }
+    values.push(this.value);
+
+    if (this.right) {
+      values = this.right.dfs(values);
+    }
+    return values;
+  }
+
+  bfs(tree, values=[]) {
+      const queue = new Queue();
+      const node = tree.root;
+      queue.enqueue(node);
+      while(queue.length){
+          const node = queue.dequeue();
+          values.push(node.value);
+
+          if(node.left) {
+              queue.enqueue(node.left);
+          }
+
+          if(node.right) {
+              queue.enqueue(node.right)
+          }
+      }
+      return values
+  }
+}
+
+module.export = BinarySearchTree;
